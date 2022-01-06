6d9639cc1b757e2ca78cbbb218e4c9d9dc2ac80e
Saifaldeen Al <44649543+Saif-aldeen@users.noreply.github.com>
2022-01-05 21:23:02 -0800

    Update ccp.go

--- ccp/ccp.go
+++ ccp/ccp.go
@@ -1,7 +1,17 @@
 package ccp
 
-func AddingUp(val int) int {
-  
-  return val * 2
+import (
+  "fmt"
+  "strconv"
+)
+
+func StrToInt(val string) int {
+  value, err := strconv.Atoi(val)
+  if err != nil {
+    fmt.Println("this is invalid entry")
+  }else
+  {
+    return value
+  }
   
 }
